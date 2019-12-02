9b41aeb149518fb62356ed587060fe6d2014ae1b
Romain Guilloteau <romain.guilloteau@telecom-paristech.fr>
2019-12-02 10:20:19 +0100

    V2 Simulation TODO: Deal with memory leak issue

---
+++ LitSimulate/cpp_version/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QPushButton * fileExplorer = this->findChild<FileExplorer *>();
+    connect(fileExplorer, SIGNAL(file_chosen(QString)), this, SLOT(receive_file(QString)));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::receive_file(QString file){
+    emit file_transmit(file);
+}
