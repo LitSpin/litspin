fd1cfe6f7fc96c27809bca3b6a2b291bd18db1de
Romain Guilloteau <romain.guilloteau@telecom-paristech.fr>
2020-02-04 17:46:25 +0100

    WIP

---
+++ FPGA/quartus/LitFPGA/LitFPGA_inst.v
@@ -0,0 +1,40 @@
+	LitFPGA u0 (
+		.clk_clk                             (<connected-to-clk_clk>),                             //                       clk.clk
+		.memory_mem_a                        (<connected-to-memory_mem_a>),                        //                    memory.mem_a
+		.memory_mem_ba                       (<connected-to-memory_mem_ba>),                       //                          .mem_ba
+		.memory_mem_ck                       (<connected-to-memory_mem_ck>),                       //                          .mem_ck
+		.memory_mem_ck_n                     (<connected-to-memory_mem_ck_n>),                     //                          .mem_ck_n
+		.memory_mem_cke                      (<connected-to-memory_mem_cke>),                      //                          .mem_cke
+		.memory_mem_cs_n                     (<connected-to-memory_mem_cs_n>),                     //                          .mem_cs_n
+		.memory_mem_ras_n                    (<connected-to-memory_mem_ras_n>),                    //                          .mem_ras_n
+		.memory_mem_cas_n                    (<connected-to-memory_mem_cas_n>),                    //                          .mem_cas_n
+		.memory_mem_we_n                     (<connected-to-memory_mem_we_n>),                     //                          .mem_we_n
+		.memory_mem_reset_n                  (<connected-to-memory_mem_reset_n>),                  //                          .mem_reset_n
+		.memory_mem_dq                       (<connected-to-memory_mem_dq>),                       //                          .mem_dq
+		.memory_mem_dqs                      (<connected-to-memory_mem_dqs>),                      //                          .mem_dqs
+		.memory_mem_dqs_n                    (<connected-to-memory_mem_dqs_n>),                    //                          .mem_dqs_n
+		.memory_mem_odt                      (<connected-to-memory_mem_odt>),                      //                          .mem_odt
+		.memory_mem_dm                       (<connected-to-memory_mem_dm>),                       //                          .mem_dm
+		.memory_oct_rzqin                    (<connected-to-memory_oct_rzqin>),                    //                          .oct_rzqin
+		.reset_reset_n                       (<connected-to-reset_reset_n>),                       //                     reset.reset_n
+		.led_band_controller_0_hps_sout      (<connected-to-led_band_controller_0_hps_sout>),      // led_band_controller_0_hps.sout
+		.led_band_controller_0_hps_fc_clk    (<connected-to-led_band_controller_0_hps_fc_clk>),    //                          .fc_clk
+		.led_band_controller_0_hps_fc_data   (<connected-to-led_band_controller_0_hps_fc_data>),   //                          .fc_data
+		.led_band_controller_0_hps_override  (<connected-to-led_band_controller_0_hps_override>),  //                          .override
+		.led_band_controller_0_hps_new_frame (<connected-to-led_band_controller_0_hps_new_frame>), //                          .new_frame
+		.hps_io_hps_io_sdio_inst_CMD         (<connected-to-hps_io_hps_io_sdio_inst_CMD>),         //                    hps_io.hps_io_sdio_inst_CMD
+		.hps_io_hps_io_sdio_inst_D0          (<connected-to-hps_io_hps_io_sdio_inst_D0>),          //                          .hps_io_sdio_inst_D0
+		.hps_io_hps_io_sdio_inst_D1          (<connected-to-hps_io_hps_io_sdio_inst_D1>),          //                          .hps_io_sdio_inst_D1
+		.hps_io_hps_io_sdio_inst_D4          (<connected-to-hps_io_hps_io_sdio_inst_D4>),          //                          .hps_io_sdio_inst_D4
+		.hps_io_hps_io_sdio_inst_D5          (<connected-to-hps_io_hps_io_sdio_inst_D5>),          //                          .hps_io_sdio_inst_D5
+		.hps_io_hps_io_sdio_inst_D6          (<connected-to-hps_io_hps_io_sdio_inst_D6>),          //                          .hps_io_sdio_inst_D6
+		.hps_io_hps_io_sdio_inst_D7          (<connected-to-hps_io_hps_io_sdio_inst_D7>),          //                          .hps_io_sdio_inst_D7
+		.hps_io_hps_io_sdio_inst_CLK         (<connected-to-hps_io_hps_io_sdio_inst_CLK>),         //                          .hps_io_sdio_inst_CLK
+		.hps_io_hps_io_sdio_inst_D2          (<connected-to-hps_io_hps_io_sdio_inst_D2>),          //                          .hps_io_sdio_inst_D2
+		.hps_io_hps_io_sdio_inst_D3          (<connected-to-hps_io_hps_io_sdio_inst_D3>),          //                          .hps_io_sdio_inst_D3
+		.hps_io_hps_io_gpio_inst_GPIO49      (<connected-to-hps_io_hps_io_gpio_inst_GPIO49>),      //                          .hps_io_gpio_inst_GPIO49
+		.hps_io_hps_io_gpio_inst_GPIO50      (<connected-to-hps_io_hps_io_gpio_inst_GPIO50>),      //                          .hps_io_gpio_inst_GPIO50
+		.hps_io_hps_io_gpio_inst_GPIO51      (<connected-to-hps_io_hps_io_gpio_inst_GPIO51>),      //                          .hps_io_gpio_inst_GPIO51
+		.hps_io_hps_io_gpio_inst_GPIO52      (<connected-to-hps_io_hps_io_gpio_inst_GPIO52>)       //                          .hps_io_gpio_inst_GPIO52
+	);
+
